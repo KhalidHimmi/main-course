1f978d6bf506e316b477a2a8bdd133cc17f9f837
Khalid <73300902+KhalidHimmi@users.noreply.github.com>
2024-05-19 18:30:24 +0100

    Connect between angular and express js,then allow the communication between them

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import * as uuid from "uuid";
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
     private posts: Post[] = [];
     private postsUpdated = new Subject<Post[]>();
 
+    constructor(private http: HttpClient) { }
+
     getPosts() {
-        return [...this.posts];
+        this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
+            .subscribe((postData) => {
+                this.posts = postData.posts;
+                this.postsUpdated.next([...this.posts]);
+            })
     }
 
     getPostUpdateListener() {
@@ -17,6 +25,7 @@ export class PostService {
 
     addPost(title: string, content: string) {
         const post: Post = {
+            id: uuid.v4(),
             title: title,
             content: content
         }
